2bed2b1e32b01cf01c1f3e75ebe57f3c8e65d736
SmokeySmoke <35714295+SmokeySmoke@users.noreply.github.com>
2018-04-13 21:42:27 -0400

    add annuity formula

--- FinanceCalculator.c
+++ FinanceCalculator.c
@@ -9,7 +9,7 @@
 
 float simpleInterestCalc(float, float, float);
 float compInterestCalc(char, float, float, float, float);
-void annuitiesCalc(void);
+float annuitiesCalc(char, char, float, float, float, float);
 void menu(void);
 void flush(void);
 float getfloat(void);
@@ -19,7 +19,7 @@ int main(void)
 	char choice;
 	unsigned i;
 	const int compChoices[7] = {1,2,4,12,26,56,365};
-	float investment, rate, years, interest, comp;
+	float investment, rate, years, interest, comp, pay, value;
 	_Bool isComp = 0;
 
 	puts("FINANCE CALCULATOR\n");
@@ -100,7 +100,84 @@ int main(void)
 						  "Interest:     $%.2f\n\n", interest, investment - interest);
 				   flush();
 				   break;
-			case '3' : annuitiesCalc();
+			case '3' : puts("ANNUITY PAYMENT CALCULATOR\n"
+					"1. What an annuity will be worth in the future\n"
+					"2. What the principle value of an annuity is worth\n"
+					"3. What regular payments in an annuity will cost per payment\n");
+				   while ((choice = getchar()) < '1' || choice > '3')
+				   {
+					   puts("Invalid selection. Choose 1, 2 or 3:");
+					   flush();
+				   }
+				   getchar();
+
+			           char valchoice;
+				   if (choice == '3')
+				   {
+					   puts("Choose the value you have available:\n"
+					        "1. Future value\n"
+						"2. Present value");
+					   while ((valchoice = getchar()) != '1' && valchoice != '2')
+					   {
+						   puts("Invalid selection. Choose 1 or 2:");
+						   flush();
+					   }
+					   if (valchoice == '1')
+						   printf("Future value: $");
+					   else if (valchoice == '2')
+						   printf("Present value: $");
+					   else
+					   {
+						   puts("Programming error in Annuity value choice!");
+						   exit(EXIT_FAILURE);
+					   }
+				   } else 
+				   {
+					valchoice = '0';
+				   	puts("Cost per payment:");
+				   }
+				   pay = getfloat();
+
+				   puts("Rate:");
+				   rate = getfloat();
+
+				   puts("Compound period:\n"
+					"Annually      -   1\n"
+					"Semi-Annually -   2\n"
+					"Quarterly     -   4\n"
+					"Monthly       -  12\n"
+					"Bi-Weekly     -  26\n"
+					"Weekly        -  52\n"
+					"Daily         - 365");
+				   while (scanf("%f", &comp) != 1 || !isComp)
+				   {
+					   for (i = 0; i < sizeof compChoices / sizeof(int); i++)
+						   if (comp == compChoices[i])
+							   isComp = 1;
+					   if (isComp)
+						   break;
+					   puts("Invalid selection.");
+					   flush();
+				   }
+				   if (choice == '1')
+					   puts("Years to invest:");
+				   else
+					   puts("Years invested:");
+				   years = getfloat();
+
+
+				   value = annuitiesCalc(choice, valchoice, pay, rate, comp, years);
+				   putchar('\n');
+				   if (choice == '1')
+					   printf("Future value: $%.2f\n", value);
+				   else if (choice == '2')
+					   printf("Present value: $%.2f\n", value);
+				   else if (choice == '3')
+					   printf("Payment cost: $%.2f\n", value);
+				   else
+					   exit(EXIT_FAILURE);
+				   putchar('\n');
+				   flush();
 				   break;
 			default  : puts("\nInvalid selection.\n");
 		}
@@ -133,10 +210,16 @@ float compInterestCalc(char choice, float inv, float rate, float cmpPeriod, floa
 	}
 }
 
-void annuitiesCalc(void)	// annuity payments formula
+float annuitiesCalc(char choice, char valchoice, float pay, float rate, float compP, float years)	// annuity payments formula
 {
-	puts("\nANNUITY PAYMENT CALCULATOR\n");
-	return;
+	float i = (rate/100) / compP;
+	float n = compP * years;
+	float c = pow(i+1, choice == '2' || valchoice == '2' ? -n : n);
+
+	if (choice == '3')
+		return pay * (i / (valchoice == '1' ? c-1 : 1-c));
+	else
+		return pay * ((choice == '1' ? c-1 : 1-c) / i);
 }
 
 void menu(void)
